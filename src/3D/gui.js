a3d5f3cccbc5ed059b6576210ea240f1b1504ed6
bkcastro <c1brandon626@gmail.com>
2026-05-19 16:01:02 -0700

    rebase

--- src/3D/gui.js
+++
@@ -1,8 +0,0 @@
-// src/gui.js
-import GUI from 'lil-gui';
-
-const gui = new GUI();
-
-gui.hide();
-
-export default gui;
--- src/images.js
+++
@@ -1,5 +0,0 @@
-
-export const images = [
-    {
-        src: ,
-        caption: "",
